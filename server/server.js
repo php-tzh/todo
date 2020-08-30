0033f1b3eeaae555c01554b18a2bec4715c96736
tzh <2197486242@qq.com>
2020-08-31 06:44:19 +0800

    静态资源处理

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const Koa = require('koa')
+const path = require('path')
 
 const app = new Koa()
 const pageRouter = require('./routers/dev-ssr')
-
+const staticRouter = require('./routers/static')
+const Router = require('koa-router');
+const send = require('koa-send');
 const isDev = process.env.NODE_ENV === 'development'
-
+let router = new Router()
 //全局异常处理
 app.use(async (ctx,next)=>{
     try{
@@ -22,6 +25,19 @@ app.use(async (ctx,next)=>{
         }
     }
 })
+// favicon.ico 处理
+app.use(async (ctx,next)=>{
+    if(ctx.path==='/favicon.ico'){
+      await  send(ctx,'/favicon.ico',{root:path.join(__dirname,'../')})
+    }else{
+        await next()
+    }
+})
+//静态资源
+staticRouter(router,{
+    html:365
+});
+app.use(router.routes())
 
 app.use(pageRouter.routes()).use(pageRouter.allowedMethods())
 
